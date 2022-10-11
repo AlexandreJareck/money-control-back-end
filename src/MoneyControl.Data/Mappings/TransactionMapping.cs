1a1462a7c40721901a08fdab05cacb2a300050ea
AlexandreJareck <alexandrejareck@gmail.com>
2022-10-11 20:12:23 -0300

    feat: created mapping to columns

---
+++ src/MoneyControl.Data/Mappings/TransactionMapping.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MoneyControl.Business.Models;
+
+namespace MoneyControl.Data.Mappings;
+
+public class TransactionMapping : IEntityTypeConfiguration<Transaction>
+{
+    public void Configure(EntityTypeBuilder<Transaction> builder)
+    {
+        builder.HasKey(t => t.Id);
+
+        builder.Property(p => p.Description)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(p => p.Category)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Property(p => p.Type)
+            .IsRequired()
+            .HasMaxLength(50);
+    }
+}

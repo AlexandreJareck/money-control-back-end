7f3bb2d17b675259f91e8f5d575120ebf30f9c6c
AlexandreJareck <alexandrejareck@gmail.com>
2022-10-11 22:11:38 -0300

    feat: created auto mapper config

---
+++ src/MoneyControl.App/Configuration/AutoMapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using MoneyControl.App.DTOs;
+using MoneyControl.Business.Models;
+
+namespace MoneyControl.App.Configuration
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            CreateMap<Transaction, TransactionDTO>().ReverseMap();
+        }
+    }
+}
--- src/MoneyControl.App/Program.cs
+++ src/MoneyControl.App/Program.cs
@@ -12,6 +12,8 @@ builder.Services.AddDbContext<MoneyControlDbContext>(x =>
     x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())

05005b0538405dbc25e09ab79b4b8f4ed9ab029c
AlexandreJareck <alexandrejareck@gmail.com>
2022-10-11 22:08:17 -0300

    feat: created transaction dto

---
+++ src/MoneyControl.App/DTOs/TransactionDTO.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MoneyControl.App.DTOs
+{
+    public class TransactionDTO
+    {
+        public const string ErrorMessage = "Campo {0} é obrigatório!";
+
+        public TransactionDTO(Guid id)
+        {
+            Id = id;
+            CreatedAt = DateTime.Now;
+        }
+
+
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = ErrorMessage)]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracateres", MinimumLength = 5)]
+        public string? Description { get; set; }
+
+        [Required(ErrorMessage = ErrorMessage)]
+        [StringLength(50, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracateres", MinimumLength = 2)]
+        public string? Type { get; set; }
+
+        [Required(ErrorMessage = ErrorMessage)]
+        [StringLength(50, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracateres", MinimumLength = 2)]
+        public string? Category { get; set; }
+
+        [Required(ErrorMessage = ErrorMessage)]
+        public decimal? Price { get; set; }
+
+        
+        public DateTime? CreatedAt { get; set; }
+    }
+}

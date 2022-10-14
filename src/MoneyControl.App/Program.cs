8d4205d760e50cbcf57f7280987f9bae8564a6b1
AlexandreJareck <alexandrejareck@gmail.com>
2022-10-13 23:04:28 -0300

    feat: add cors

--- src/MoneyControl.App/Program.cs
+++ src/MoneyControl.App/Program.cs
@@ -17,6 +17,29 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.ResolveDependencies();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("Development", builder =>
+        builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+
+    options.AddDefaultPolicy(builder =>
+        builder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+
+    options.AddPolicy("Production", builder =>
+        builder
+            .WithMethods("GET")
+            .WithOrigins("http://meusite.io")
+            .SetIsOriginAllowedToAllowWildcardSubdomains()
+            //.WithHeaders(HeaderNames.ContentType, "x-custom-header")
+            .AllowAnyHeader());
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
